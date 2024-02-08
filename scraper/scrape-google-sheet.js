fed15beba121ed69e7ae1fa6b0096fffe27e6655
Marc Fehr <marc.fehr@gmail.com>
2024-02-08 18:02:16 +0200

    adjust main script

--- scraper/scrape-google-sheet.js
+++ scraper/scrape-google-sheet.js
@@ -1,92 +1,25 @@
 const path = require('path');
 const fs = require('fs');
-const sharp = require('sharp');
 
-/* Import csvHandler module and set csvUrl */
+// Import necessary modules
 const { fetchAndParseCSV } = require('./modules/csvHandler');
+const { createThumbnail, isImage } = require('./modules/imageProcessor');
+const slugify = require('./modules/slugify'); // Adjust the path based on
+const saveToJson = require('./modules/saveToJson'); // Adjust the path based on your directory structure
+const placeholderImageArray = require('./data/placeholderImages');
+const processWhatsAppAttachments = require('./modules/processWhatsAppAttachments'); // Adjust the path as necessary
+
 const csvUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSo1JkBPpgo-jq5HbgZhdrWZ8lDGI8vF0C30gHPweWebwoKJbsmuKtED07jLqSDz3zpZMAfBpFl_Khv/pub?output=csv'; // Replace with your published CSV URL
 
-const placeholderImageArray = [
-  '../images/placeholder_images/puzzle-01.png',
-  '../images/placeholder_images/puzzle-02.png',
-  '../images/placeholder_images/puzzle-03.png',
-  '../images/placeholder_images/puzzle-04.png',
-  '../images/placeholder_images/puzzle-05.png',
-  '../images/placeholder_images/puzzle-06.png',
-  '../images/placeholder_images/puzzle-07.png',
-  '../images/placeholder_images/puzzle-08.png',
-  '../images/placeholder_images/puzzle-09.png',
-  '../images/placeholder_images/puzzle-10.png',
-  '../images/placeholder_images/puzzle-11.png',
-  '../images/placeholder_images/puzzle-12.png',
-  '../images/placeholder_images/puzzle-13.png',
-  '../images/placeholder_images/puzzle-14.png',
-  '../images/placeholder_images/puzzle-15.png',
-  '../images/placeholder_images/puzzle-16.png',
-  '../images/placeholder_images/puzzle-17.png',
-  '../images/placeholder_images/puzzle-18.png',
-  '../images/placeholder_images/puzzle-19.png',
-  '../images/placeholder_images/puzzle-20.png',
-  '../images/placeholder_images/puzzle-21.png',
-  '../images/placeholder_images/puzzle-22.png',
-  '../images/placeholder_images/puzzle-23.png',
-  '../images/placeholder_images/puzzle-24.png',
-  '../images/placeholder_images/puzzle-25.png',
-  '../images/placeholder_images/puzzle-26.png',
-  '../images/placeholder_images/puzzle-27.png',
-  '../images/placeholder_images/puzzle-28.png',
-  '../images/placeholder_images/puzzle-29.png',
-  '../images/placeholder_images/puzzle-30.png',
-  '../images/placeholder_images/puzzle-31.png',
-  '../images/placeholder_images/puzzle-32.png',
-]
-
-async function createThumbnail(sourcePath, destinationPath) {
-  try {
-    await sharp(sourcePath)
-      .resize(400, 400) // Resize the image to 400x400
-      .toFile(destinationPath);
-    console.log(`Thumbnail created: ${destinationPath}`);
-
-    // Convert the full path to a relative path
-    const relativePath = destinationPath.replace('/Users/marcfehr/Sites/tnh-storytelling-astro-yemen/public', '..');
-    return relativePath;
-  } catch (err) {
-    console.error('Error creating thumbnail:', err);
-    return null;
-  }
-}
-
-function isImage(file) {
-  const imageExtensions = ['.jpg', '.jpeg', '.png', '.heic'];
-  return imageExtensions.includes(path.extname(file).toLowerCase());
-}
-
-/* Fetch data */
+// Fetch data
 fetchAndParseCSV(csvUrl)
   .then(data => {
-    console.log('CSV Data:', data);
+    // Process data once fetched
     processData(data)
   })
   .catch(error => {
     console.error('An error occurred:', error);
   });
-
-function slugify(text) {
-  return text.toString().toLowerCase()
-    .replace(/\s+/g, '-')           // Replace spaces with -
-    .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
-    .replace(/\-\-+/g, '-')         // Replace multiple - with single -
-    .replace(/^-+/, '')             // Trim - from start of text
-    .replace(/-+$/, '');            // Trim - from end of text
-}
-
-function saveToJson(data) {
-  fs.writeFile('output.json', JSON.stringify(data, null, 2), (err) => {
-    if (err) return console.error('Error saving JSON:', err);
-    console.log('Data saved to output.json');
-  });
-}
 
 async function processData(data) {
   const processedData = [];
@@ -103,51 +36,88 @@ async function processData(data) {
     let attachments = entry.Attachments && entry.Attachments.trim() && entry.Attachments.trim() !== '-'
       ? entry.Attachments.split(',').map(file => file.trim())
       : [];
-    let storyImage = placeholderImageArray[Math.floor(Math.random() * placeholderImageArray.length)];
 
+    // Find and use the first image attachment as the story image
+    let storyImage = placeholderImageArray[Math.floor(Math.random() * placeholderImageArray.length)];
     const imageAttachments = attachments.filter(isImage);
 
+    // Process attachments that came through MachForm
     if (entry['Method of submission'] === 'MachForm' && imageAttachments.length > 0) {
+
       const firstImage = imageAttachments[0];
       const sourceFilePath = path.join(__dirname, './assets/machform_assets/', firstImage);
       const thumbnailPath = path.join(__dirname, '../public/images/thumbnails/', `thumbnail-${entry.ID}.jpg`);
-      // storyImage = `../images/thumbnails/thumbnail-${entry.ID}.jpg`
-      const thumbnailResult = await createThumbnail(sourceFilePath, thumbnailPath);
 
-      if (thumbnailResult) {
-        // console.log('Thumbnail result:', thumbnailResult)
-        storyImage = thumbnailResult; // Update storyImage to the thumbnail path
+      try {
+        const thumbnailResult = await createThumbnail(sourceFilePath, thumbnailPath);
+        if (thumbnailResult) {
+          // Use `thumbnailResult` for further processing
+          // For example, updating `storyImage` variable
+          storyImage = thumbnailResult; // Assuming you're doing something with this variable next
+        } else {
+          console.log('Failed to create thumbnail.');
+        }
+      } catch (error) {
+        console.error('An error occurred while creating a thumbnail:', error);
       }
+
+      // Process all attachments for MachForm entry
+      let attachmentCounter = 1;
+      attachments = attachments.map(file => {
+        let extension = path.extname(file).toLowerCase();
+        let newFilename = `${entry.ID}-${attachmentCounter}${extension}`;
+        const sourceFilePath = path.join(__dirname, './assets/machform_assets/', file);
+        const newFilePath = path.join(__dirname, '../src/assets/', newFilename);
+
+        if (extension === '.heic') {
+          // Rename .heic to .jpg
+          newFilename = `${entry.ID}-${attachmentCounter}.jpg`;
+          const jpgFilePath = path.join(__dirname, '../src/assets/', newFilename);
+          try {
+            fs.renameSync(sourceFilePath, jpgFilePath);
+          } catch (err) {
+            console.error('Error renaming HEIC to JPG:', err);
+          }
+        } else {
+          try {
+            fs.copyFileSync(sourceFilePath, newFilePath);
+          } catch (err) {
+            console.error('Error copying file:', err);
+          }
+        }
+
+        attachmentCounter++;
+        return newFilename;
+      });
     }
 
-    let attachmentCounter = 1;
-    attachments = attachments.map(file => {
-      let extension = path.extname(file).toLowerCase();
-      let newFilename = `${entry.ID}-${attachmentCounter}${extension}`;
-      const sourceFilePath = path.join(__dirname, './assets/machform_assets/', file);
-      const newFilePath = path.join(__dirname, '../src/assets/', newFilename);
 
-      if (extension === '.heic') {
-        // Rename .heic to .jpg
-        newFilename = `${entry.ID}-${attachmentCounter}.jpg`;
-        const jpgFilePath = path.join(__dirname, '../src/assets/', newFilename);
-        try {
-          fs.renameSync(sourceFilePath, jpgFilePath);
-        } catch (err) {
-          console.error('Error renaming HEIC to JPG:', err);
+
+    // Process WhatsApp attachments
+    if (entry['Method of submission'] === 'WhatsApp') {
+      const entryID = entry.ID;
+      const whatsappAssetsDir = path.join(__dirname, `./assets/whatsapp_assets/${entryID}`);
+      let files = [];
+
+      try {
+        // Attempt to read the directory contents
+        files = fs.readdirSync(whatsappAssetsDir);
+
+        // If successful, files array is now populated with filenames
+        if (files.length > 0) {
+          // Proceed to process these files
+          const processedFiles = processWhatsAppAttachments(entryID, files, __dirname);
+          console.log(`Processed files for entry ${entryID}:`, processedFiles);
         }
-      } else {
-        try {
-          fs.copyFileSync(sourceFilePath, newFilePath);
-        } catch (err) {
-          console.error('Error copying file:', err);
+      } catch (error) {
+        if (error.code === 'ENOENT') {
+          // Directory does not exist
+          console.log(`No directory found for WhatsApp entry ${entryID}, proceeding with an empty array.`);
+        } else {
+          console.error(`Error accessing WhatsApp assets for entry ${entryID}:`, error);
         }
       }
-
-      attachmentCounter++;
-      return newFilename;
-    });
-
+    }
     processedData.push({
       id: entry.ID,
       slugs: {
