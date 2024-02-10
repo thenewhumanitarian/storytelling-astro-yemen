1a328aaa5c08c19dbac1a224f3f77b9736a27cca
Marc Fehr <marc.fehr@gmail.com>
2024-02-10 08:50:35 +0200

    adds nav buttons

---
+++ src/data/translations.js
@@ -0,0 +1,46 @@
+export const translations = {
+  back: {
+    ar: 'رجوع',
+    en: 'Back'
+  },
+  next: {
+    ar: 'التالي',
+    en: 'Next'
+  },
+  previous: {
+    ar: 'السابق',
+    en: 'Previous'
+  },
+  overview: {
+    ar: 'نظرة عامة',
+    en: 'Overview'
+  },
+  images: {
+    ar: 'صور',
+    en: 'Images'
+  },
+  videos: {
+    ar: 'فيديو',
+    en: 'Videos'
+  },
+  documents: {
+    ar: 'مستندات',
+    en: 'Documents'
+  },
+  name: {
+    ar: 'الاسم',
+    en: 'Name'
+  },
+  age: {
+    ar: 'العمر',
+    en: 'Age'
+  },
+  from: {
+    ar: 'من',
+    en: 'From'
+  },
+  livingIn: {
+    ar: 'يعيش في',
+    en: 'Living in'
+  },
+}
