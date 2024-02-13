02a0301e05434deea0e964d2e8e114c1923f097a
Marc Fehr <marc.fehr@gmail.com>
2024-02-13 21:11:10 +0200

    prefetch and page transitions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-// import prefetch from '@astrojs/prefetch';
-// import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  // output: 'server',
-  // adapter: vercel(),
   prefetch: {
     prefetchAll: true
   },
   integrations: [tailwind({
     applyBaseStyles: false
-  })
+  })]
 });
