2f221462c36f54a5780ebcb6e5d709570874977f
Marc Fehr <marc.fehr@gmail.com>
2024-03-11 12:57:05 +0200

    language buttons in menu overlay

--- src/stores/globalState.js
+++ src/stores/globalState.js
@@ -5,4 +5,4 @@ import { persistentAtom } from '@nanostores/persistent'
 export const isMenuOverlayVisible = atom(false)
 
 export const locale = persistentAtom('locale', 'en')
-export const firstTimeVisitor = persistentAtom('showIntro', true)
+export const firstTimeVisitor = persistentAtom('hideIntro', true)
