6b7c7b33d825d250934dbe11e1bbac23814be072
Marc Fehr <marc.fehr@gmail.com>
2024-03-24 15:30:54 +0200

    remove babel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import partytown from '@astrojs/partytown'
 import react from "@astrojs/react";
-import babelInlineScripts from "astro-babel-inline-scripts";
 
 // https://astro.build/config
 export default defineConfig({
@@ -27,27 +26,5 @@ export default defineConfig({
       },
     }),
     react(),
-    babelInlineScripts(
-      {
-        presets: [
-          [
-            "@babel/env",
-            {
-              targets: {
-                browsers: ["> .5% or last 2 versions"],
-              },
-            },
-          ],
-        ],
-      },
-      (route) => {
-        // Disable processing for index page.
-        if (route === "/") {
-          return false;
-        }
-
-        return true;
-      }
-    ),
   ]
 });
